4eebc4b8b20780c20b003e849b6882cddc25d6f8
Daniel Casalli <danis.settk@gmail.com>
2026-06-12 09:57:56 -0300

    muda server

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import 'dotenv/config';
-import exemplosRoutes from './routes/exemploRoute.js';
+import alunosRoutes from './routes/alunosRoute.js';
 import { apiKey } from './lib/middlewares/apiKey.js'
 import arquivoRoutes from './routes/arquivoRoute.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;  
 
 app.use(express.json());
 
-app.use('/api/exemplos', apiKey, exemplosRoutes);
+app.use('/api/alunos', apiKey, alunosRoutes);
 
-app.use('/api/exemplos', apiKey, arquivoRoutes);
+app.use('/api/alunos', apiKey, arquivoRoutes);
 
 app.get('/', (req, res) => {
     res.send('🚀 API funcionando');
 });
 
 // Rotas
-app.use('/api/exemplos', exemplosRoutes);
+app.use('/api/alunos', alunosRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
