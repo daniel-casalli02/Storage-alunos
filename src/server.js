60897a5c743a2d3541bbd4c4fcb6b8cb514cc2ad
Daniel Casalli <danis.settk@gmail.com>
2026-06-12 10:27:10 -0300

    fix: arruma rota

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import 'dotenv/config';
 import alunosRoutes from './routes/alunosRoute.js';
 import { apiKey } from './lib/middlewares/apiKey.js'
-import arquivoRoutes from './routes/arquivoRoute.js';
+import arquivoRoutes from './routes/arquivosRoute.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;  
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
